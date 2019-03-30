8db46c824524c7c1340b4f38eaf4966666b44883
Paul Matthew Barrameda <luisanduis729@gmail.com>
2019-03-30 13:50:20 -0700

    Added user location and lock/unlock

--- LAHacks2019/LocatorViewController.swift
+++ LAHacks2019/LocatorViewController.swift
@@ -9,24 +9,57 @@
 import UIKit
 import MapKit
 
-class LocatorViewController: UIViewController {
-    @IBOutlet weak var lockUnlockLabel: UILabel!
-    @IBOutlet weak var Map: MKMapView!
+class LocatorViewController: UIViewController, CLLocationManagerDelegate {
+  let locationManager = CLLocationManager()
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var lockUnlockLabel: UILabel!
+  @IBOutlet weak var map: MKMapView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func lockUnlockSwitch(_ sender: Any) {
+    if lockUnlockLabel.text == "Unlocked" {
+      lockUnlockLabel.text = "Locked"
+    } else {
+      lockUnlockLabel.text = "Unlocked"
+    }
+  }
+    
+  @IBAction func locate(_ sender: Any) {
+    // If no location services are enabled, prompt user to enable it.
+    if !CLLocationManager.locationServicesEnabled() {
+      print("No services enabled")
+      return
     }
     
-    @IBAction func lockUnlockSwitch(_ sender: Any) {
-        lockUnlockLabel.text = "Hello there"
-    }
-    
-    @IBAction func Locate(_ sender: Any) {
+    // If user still did not enable location services, skip following loop.
+    if CLLocationManager.locationServicesEnabled() {
+      print("User accepted")
+      locationManager.requestAlwaysAuthorization()
+      locationManager.delegate = self
+      locationManager.desiredAccuracy = kCLLocationAccuracyBest
+      locationManager.startUpdatingLocation()
       
+      map.showsUserLocation = true
     }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let userLocation:CLLocation = locations[0] as CLLocation
     
-    /*
+    manager.stopUpdatingLocation()
+    
+    let coordinations = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,longitude: userLocation.coordinate.longitude)
+    let span = MKCoordinateSpan.init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    let region = MKCoordinateRegion(center: coordinations, span: span)
+    
+    map.setRegion(region, animated: true)
+  }
+  
+  /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation

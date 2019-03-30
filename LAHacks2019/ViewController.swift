a5920f8ee0e0dd1fc31877096bfbed047be3df32
Chris Rodriguez <chrismail627@gmail.com>
2019-03-30 00:29:42 -0700

    First commit.

---
+++ LAHacks2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LAHacks2019
+//
+//  Created by Chris Rodriguez on 3/30/19.
+//  Copyright © 2019 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
